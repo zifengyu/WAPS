ca786e7ccb706b6a567abb6096529bb51aa49986
Alan Yu <ayu@splunk.com>
2015-11-05 15:42:02 +0800

    merge all

--- waps.py
+++ waps.py
@@ -99,11 +99,12 @@ def get_proxy_list():
     l4 = proxy3.run()
     logging.info('[ProxyList] proxy4={0}'.format(len(l4)))
 
-    return l4
+    return l1 + l2 + l3 + l4
 
 
 def run():
     proxy_list = get_proxy_list()
+    logging.info('[ProxyList] total={0}'.format(len(proxy_list)))
 
     sleep_time = [7200, 7200, 7200, 7200, 7200, 3600, 360, 253, 113, 53, 21, 7, 3, 3, 5, 4, 1, 4, 1, 3, 1, 7, 11, 13, 133, 3600]
 
