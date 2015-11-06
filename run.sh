e88032e8b78632207023bfa1d13995aa642df202
Alan Yu <ayu@splunk.com>
2015-11-06 23:45:06 +0800

    Add timeout

---
+++ run.sh
@@ -0,0 +1,29 @@
+mailSend()
+{
+        mailContent="xxxx Web response time over 5 seconds"
+        echo $mailContent
+}
+timeout()
+{
+        waitfor=3
+        command=$*
+        $command &
+        commandpid=$!
+
+        ( sleep $waitfor ; kill -9 $commandpid  > /dev/null 2>&1 && mailSend ) &
+
+        watchdog=$!
+        sleeppid=$PPID
+        wait $commandpid > /dev/null 2>&1
+
+        kill $sleeppid > /dev/null 2>&1
+}
+
+#测试的函数
+
+test123()
+{
+        python waps.py
+}
+
+timeout test123
