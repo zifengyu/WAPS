ef1c74185ec18099134984c0c451d9438be7291e
Alan Yu <ayu@splunk.com>
2015-11-22 21:34:14 +0800

    sam

--- imei.py
+++ imei.py
@@ -1,8 +1,8 @@
 import random
 import csv
 
-head = '35220306405'
-device = 'SM-N7508V,samsung,720,1280,4.3'
+head = '35744505223'
+device = 'GT-I9505,samsung,1080,1920,4.3'
 
 
 def luhn_checksum(card_number):
@@ -36,7 +36,7 @@ while index < 1000:
     p = head + str(index)
     p += str(calculate_luhn(p))
     f.write(p + ',' + imsi[i][0] + ',' + device + '\n')
-    index += random.randint(3, 23) + 1
+    index += random.randint(3, 19) + 1
     i += 1
 
 f.close()
