d2d71e77c9393193593a1b508ca01b580f755163
Alan Yu <ayu@splunk.com>
2015-11-28 18:52:12 +0800

    d

--- run.sh
+++ run.sh
@@ -23,15 +23,25 @@ test123()
 {
         # python ping_jenkins.py
         python waps.py &
+        cp1=$!
         sleep 9
         python waps.py &
+        cp2=$!
         sleep 9
         python waps.py &
+        cp3=$!
         sleep 9
         python waps.py &
+        cp4=$!
         sleep 9
         python waps.py &
+        cp5=$!
         sleep 8848
+        kill -9 $cp1
+        kill -9 $cp2
+        kill -9 $cp3
+        kill -9 $cp4
+        kill -9 $cp5
 }
 
 test123
