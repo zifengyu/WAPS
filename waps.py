fa9281ffef1f492939ecb354120de35df0983d7c
Alan Yu <ayu@splunk.com>
2016-02-19 20:57:27 +0800

    d

--- waps.py
+++ waps.py
@@ -131,7 +131,7 @@ def run():
             logging.info('[Active] status={0}'.format(response.status))
 
             if response.status == 200:
-                for i in range(12):
+                for i in range(16):
                     response, content = h.request('http://app.wapx.cn/action/ad/show?' + client, 'GET')
                     logging.info('[Show] status={0}'.format(response.status))
                     if response.status == 200:
